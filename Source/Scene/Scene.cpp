7e6bd89fa04f7723307e68a6aedb177594bc654d
iarleyyyxz <josy-@JOSSIANE>
2025-06-01 14:04:21 -0300

    Scene integration

--- Source/Scene/Scene.cpp
+++ Source/Scene/Scene.cpp
@@ -4,3 +4,13 @@ void Scene::AddSceneObject(const SceneObject& object) {
     objects.push_back(object);
     std::cout << "Added SceneObject to scene '" << m_name << "'" << std::endl;
 }
+
+void Scene::Update(float deltaTime) {
+    for (auto& object : objects) {
+        object.Update(deltaTime); // Assumindo que SceneObject tem um método Update
+        // OU, se os componentes são gerenciados separadamente dentro de SceneObject:
+        // for (auto& component : object.GetComponents()) {
+        //     component->Update(deltaTime); // Assumindo que os componentes têm um método Update
+        // }
+    }
+}
